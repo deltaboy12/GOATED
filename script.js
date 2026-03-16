0c35de0e2866c2f22f766a4b5aaddcbf4f55d70e
deltaboy12 <delta1phase@gmail.com>
2026-03-16 03:14:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("homeBtn").onclick = function() {
+  window.location.href = "index.html";
+};
+
+document.getElementById("gamesBtn").onclick = function() {
+  window.location.href = "games.html";
+};
+
+document.getElementById("videosBtn").onclick = function() {
+  window.location.href = "videos.html";
+};
+
+document.getElementById("settingsBtn").onclick = function() {
+  window.location.href = "settings.html";
+};
